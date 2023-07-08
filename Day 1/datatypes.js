abf5392bb67d5d3efe90468de4d09aba24eacd9d
gregn-jr <81333109+gregn-jr@users.noreply.github.com>
2023-07-08 14:15:04 -0400

    Adding code
    adding code from day 1 of javascript course

---
+++ Day 1/datatypes.js
@@ -0,0 +1,11 @@
+
+let age = 24;
+let firstName = 'Greg';
+let Motive = 'I love coding';
+let isEngaged = true
+let dateOfBirth
+let birthTown = null
+let job = 'pharmacist', live = 'Canada'
+console.log(typeof age)
+console.log(typeof true)
+console.log(job, live, firstName)
---
+++ Day 1/variable.js
@@ -0,0 +1,7 @@
+let age = 24;
+let firstName = 'Greg';
+let Motive = 'I love coding';
+let isEngaged = true
+let dateOfBirth
+let birthTown = null
+
